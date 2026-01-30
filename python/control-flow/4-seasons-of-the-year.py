4b6e70317b564342216e53902af4892217b45695
Sonny Li <sonnynomnom@gmail.com>
2026-01-30 14:31:52 -0500

    added

---
+++ python/control-flow/4-seasons-of-the-year.py
@@ -0,0 +1,14 @@
+# 🗓️ Seasons of the Year
+
+month = int(input('What is the current month?: '))
+
+if month == 1 or month == 2 or month == 3:
+  print('Winter 🌨️')
+elif month == 4 or month == 5 or month == 6:
+  print('Spring 🌱')
+elif month == 7 or month == 8 or month == 9:
+  print('Summer 🌻')
+elif month == 10 or month == 11 or month == 12:
+  print('Autumn 🍂')
+else:
+  print('Invalid')
